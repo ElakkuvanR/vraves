5c6df0e8c7008dddbe28c277c56b103f00886c09
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-09-14 11:01:07 +0200

    fetch-environments and fetch-projects APIs are added

--- integration/sitecore-xm-cloud-vercel-integration/pages/api/fetch-xm-environments.js
+++ integration/sitecore-xm-cloud-vercel-integration/pages/api/fetch-xm-environments.js
@@ -1,5 +1,34 @@
+import { PowerShell } from "full-powershell";
+
 export default async function fetchXMEnvironments(req, res) {
-    const clientid = req.query.clientid;
-    const clientsecret = req.query.clientsecret;
-  }
-  
+  const projectId = req.query.projectid;
+
+  const powershellEt = new PowerShell();
+  const accessTokenPs = `(Get-Content "D:\\ValtechHackathon_2022\\XM-Cloud-Introduction\\.sitecore\\user.json" | ConvertFrom-Json).endpoints.xmCloud.accessToken`;
+  let accessToken;
+  await powershellEt
+    .call(accessTokenPs, "json")
+    .promise()
+    .then(
+      (result) => {
+        accessToken = result.success;
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  powershellEt.destroy();
+
+  const result = await fetch(
+    `https://xmclouddeploy-api.sitecorecloud.io/api/projects/v1/${projectId}/environments`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      method: "GET",
+    }
+  );
+  const body = await result.json();
+  console.log("Environments " + body);
+  return res.status(200).json(body);
+}
