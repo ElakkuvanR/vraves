7a94d48e45891176858540dfa836f7acc09f0659
nkdram <nkdramkum@gmail.com>
2022-09-14 22:28:40 +0200

    Setup apis for env variables

---
+++ integration/sitecore-xm-cloud-vercel-integration/pages/api/vercel/set-env-variable-for-project.js
@@ -0,0 +1,34 @@
+/**
+ * This endpoint can be only called once
+ *
+ * Our `code` is only valid for one request. If we call it more then once,
+ * we get "Invalid grant: authorization code is invalid".
+ */
+ export default async function getEnvVariableForProject(req, res) {
+  const projectId = req.query.projectId;
+  const envId = req.query.environmentId;
+  const variableKey = req.query.variableKey;
+  const variableValue = req.query.variableValue;
+
+  //get access token through vercel access code
+  const res = await fetch(`/api/get-access-token?code=${req.query.code}`)
+  const json = await res.json()
+  
+  const result = await fetch(`https://api.vercel.com/v9/projects/${projectId}/env/${envId}`, {
+    "body": {
+      "key": `${variableKey}`,
+      // "target": "<insert-value>",
+      // "type": "<insert-value>",
+      "value": `${variableValue}`
+    },
+    "headers": {
+      "Authorization": `Bearer ${json.access_token}`
+    },
+    "method": "patch"
+  })
+  const body = await result.json()
+
+  console.log(`https://api.vercel.com/v9/projects/${projectId}/env/${envId} returned:`, JSON.stringify(body, null, '  '))
+
+  res.status(200).json(body)
+}
