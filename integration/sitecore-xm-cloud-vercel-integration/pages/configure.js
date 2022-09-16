72f0b888ec13d2bb5a2ce5d952d154e99bc03ec6
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-09-16 02:30:12 +0200

    APIs are updated

--- integration/sitecore-xm-cloud-vercel-integration/pages/configure.js
+++ integration/sitecore-xm-cloud-vercel-integration/pages/configure.js
@@ -1,33 +1,53 @@
-import { useEffect, useState } from 'react'
-import GithubLogin from 'components/githublogin'
-import Layout from 'components/layout'
-import { useRouter } from 'next/router';
+import { useEffect, useContext, useState } from "react";
+import GithubLogin from "components/githublogin";
+import Layout from "components/layout";
+import { useRouter } from "next/router";
+import TokenContext from "store/token-context";
 
 // The URL of this page should be added as Configuration URL in your integration settings on Vercel
 export default function Configure() {
+  const ctx = useContext(TokenContext);
   const router = useRouter();
+  const [data, setData] = useState("");
   const params = {
-    status : ""
-  }
+    status: "",
+  };
+  const clickMeHand = () => {
+    setData("elakk");
+    console.log("1" + ctx.accesstoken);
+    console.log("2" + data);
+  };
   useEffect(() => {
     const { code } = router.query;
-    const cloneRepo = async (code) => {      
-      const res = await fetch(`/api/cloneRepository?code=${code}`);
-      params.status = "logged-in"
+    const cloneRepo = async (code) => {
+      const res = await fetch(apiUrl);
+      params.status = "logged-in";
+    };
+    let apiUrl = localStorage.getItem("apiUrl");
+    if (!apiUrl) {
+      apiUrl = `/api/git/cloneRepository?projectid=${ctx.projectid}&repo=${ctx.repourl}`;
+      localStorage.setItem("apiUrl", apiUrl);
     }
+    console.log("Clone repo called");
     if (router.isReady && code) {
-      cloneRepo(code);
+      console.log("router");
+      apiUrl = apiUrl + `&code=${code}`;
+      cloneRepo(apiUrl);
+      localStorage.removeItem("apiUrl");
     }
-  }, [router])
+  }, [router, ctx]);
 
   return (
     <Layout>
       <div className="space-y-2 text-center">
-        <h1 className="text-lg font-medium">Let us connect your repo in order to deploy to Sitecore XM Cloud</h1>
+        <h1 className="text-lg font-medium">
+          Let us connect your repo in order to deploy to Sitecore XM Cloud
+        </h1>
         <section className="py-4 flex justify-center">
-          <GithubLogin {...params}/>
+          <GithubLogin {...params} />
         </section>
+        <button onClick={clickMeHand}>Click Me</button>
       </div>
     </Layout>
-  )
+  );
 }
