8f9048a8ff7473e0c21b71fc0d7f483c07608986
Swetha-Dhanyal <swetha0504@gmail.com>
2022-09-17 22:12:03 +0200

    Logo updation

--- integration/sitecore-xm-cloud-vercel-integration/components/layout.js
+++ integration/sitecore-xm-cloud-vercel-integration/components/layout.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 
 export default function Layout(props) {
+  const logoStyle = {
+    maxWidth: "15%"
+  };
   return (
     <div className="flex flex-col min-h-screen">
       <Head>
         <title>Example Integration</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/xmcloud-vercel.ico" />
       </Head>
       <div className="flex-1 flex items-center justify-center">
         {props.children}
@@ -13,12 +16,12 @@ export default function Layout(props) {
       <footer className="mt-12 flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          <img src="/xmcloud-vercel.png" alt="XMCloud-Vercel Logo" style={logoStyle} />
         </a>
       </footer>
     </div>
