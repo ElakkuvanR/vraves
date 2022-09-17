f7e53f77dfe964d3b6065f15c851adbc5b19ed1c
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-09-18 00:42:14 +0200

    CustomHook changes

--- integration/sitecore-xm-cloud-vercel-integration/pages/callback.js
+++ integration/sitecore-xm-cloud-vercel-integration/pages/callback.js
@@ -1,57 +1,24 @@
 import { useEffect, useState, useContext } from "react";
 import { useRouter } from "next/router";
-import Layout from "components/layout";
+import useHttp from "hooks/use-http";
 import TokenContext from "store/token-context";
+import Loader from "components/UI/loader";
+import useVercelToken from "lib/vercel/get-vercel-token";
+import useVercelProjectDetails from "lib/vercel/get-vercel-project-details";
 
 export default function CallbackPage() {
   const router = useRouter();
-  const query = router.query;
+  const { sendRequest, isLoading, error } = useHttp();
   const [data, setData] = useState({});
-  const [projects, setProjects] = useState();
   const ctx = useContext(TokenContext);
-
-  useEffect(
-    () => {
-      const fetchAccessToken = async (code, project, next) => {
-        const res = await fetch(`/api/vercel/get-access-token?code=${code}`);
-        const json = await res.json();
-        setData({
-          accessToken: json.access_token,
-          userId: json.user_id,
-          teamId: json.team_id,
-        });
-        const projectResult = await fetch(
-          `/api/vercel/fetch-project-by-id?code=${code}&projectId=${project}&token=${json.access_token}`
-        );
-        const projectJson = await projectResult.json();
-        console.log("project-details",projectJson);
-        localStorage.setItem("rootDirectory",projectJson.rootDirectory);
-        const projectRepoPath = `https://${projectJson.link.type}.com/${projectJson.link.org}/${projectJson.link.repo}.git`;
-        ctx.setTokenValues(
-          code,
-          project,
-          next,
-          json.access_token,
-          projectRepoPath
-        );
-        console.log("ctx pid", ctx.projectid);
-        console.log("ctx code", ctx.code);
-        router.push("/configure");
-      };
-      if (router.isReady) {
-        const { code, currentProjectId, next } = router.query;
-        if (!data.accessToken) {
-          fetchAccessToken(code, currentProjectId, next);
-        }
-      }
-    },
-    [router],
-    [data]
-  );
-
+  const { accessToken } = useVercelToken();
+  const click = () => {
+    console.log("accessToken" + accessToken);
+  };
   return (
-    <Layout>
-      
-    </Layout>
+    <>
+      {/* <Loader text="Please hold on for a moment while we setup things in background !!!"></Loader> */}
+      <button onClick={click}>Click me</button>
+    </>
   );
 }
