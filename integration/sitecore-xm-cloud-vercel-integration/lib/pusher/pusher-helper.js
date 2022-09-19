52b18c8bf095826e693df89bedd0b74a6cb5ffb1
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-09-19 23:02:19 +0200

    Pusher changes

--- integration/sitecore-xm-cloud-vercel-integration/lib/pusher/pusher-helper.js
+++ integration/sitecore-xm-cloud-vercel-integration/lib/pusher/pusher-helper.js
@@ -1,15 +1,24 @@
 import axios from "axios";
 
 function sendErrorMessage(channel, message) {
-    axios.post(`${process.env.HOST}/api/pusher?projectid=${channel}`, { message: message.toString(), type: "error" });
+  axios.post(`${process.env.HOST}/api/pusher?projectid=${channel}`, {
+    message: message.toString(),
+    type: "error",
+  });
 }
 
 function sendInfoMessage(channel, message) {
-    axios.post(`${process.env.HOST}/api/pusher?projectid=${channel}`, { message: message.toString(), type: "info" });
+  axios.post(`${process.env.HOST}/api/pusher?projectid=${channel}`, {
+    message: message.toString(),
+    type: "info",
+  });
 }
 
 function sendSuccessMessage(channel, message) {
-    axios.post(`${process.env.HOST}/api/pusher?projectid=${channel}`, { message: message.toString(), type: "success" });
+  axios.post(`${process.env.HOST}/api/pusher?projectid=${channel}`, {
+    message: message.toString(),
+    type: "success",
+  });
 }
 
-export default { sendErrorMessage, sendInfoMessage, sendSuccessMessage }
+export { sendErrorMessage, sendInfoMessage, sendSuccessMessage };
